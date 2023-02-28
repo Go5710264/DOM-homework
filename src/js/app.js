8fef2b3b10e04b3f2f2c733408a7638647685e65
Dima Shirkov <dimashirkov@list.ru>
2023-02-28 14:28:26 +0300

    add DOMContentLoaded

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,19 @@
 import PlayingField from './playing-field/PlayingField'; // импорт класса PlayingField
 
-const round = new PlayingField(); // помещение всех ячеек в конструктор
-round.startGame();
+document.addEventListener('DOMContentLoaded', (event) => {
+  const round = new PlayingField(); // помещение всех ячеек в конструктор
+  round.startGame();
 
-const timerId = setInterval(() => {
-  if (round.mask.classList.contains('hide-mask')) {
-    round.mask.classList.remove('hide-mask');
+  const timerId = setInterval(() => {
+    if (round.mask.classList.contains('hide-mask')) {
+      round.mask.classList.remove('hide-mask');
+      return round.showMask();
+    }
+    if (round.newScore.miss.textContent === '5') {
+      clearTimeout(timerId);
+      return alert('Вы проиграли!');
+    }
+    round.newScore.addMiss(); // добавление пропуска маски
     return round.showMask();
-  }
-  if (round.newScore.miss.textContent === '5') {
-    clearTimeout(timerId);
-    return alert('Вы проиграли!');
-  }
-  round.newScore.addMiss(); // добавление пропуска маски
-  return round.showMask();
-}, 1000);
+  }, 1000);
+});
